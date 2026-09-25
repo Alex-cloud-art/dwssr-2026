5fb9a4b22acea86c53124e014c32f6e26627afcd
Alex-cloud-art <144040475+Alex-cloud-art@users.noreply.github.com>
2026-09-25 04:49:07 +0000

    refactor: migra el proyecto a ES modules (import/export en app.js, bin/www y routes)

--- webservicessr/routes/index.js
+++ webservicessr/routes/index.js
@@ -1,9 +1,1 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+import express from 'express'; const router = express.Router(); router.get('/', function(req, res, next) { res.render('index', { title: 'Express' }); }); export default router; 
--- webservicessr/routes/users.js
+++ webservicessr/routes/users.js
@@ -1,9 +1,1 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+import express from 'express'; const router = express.Router(); router.get('/', function(req, res, next) { res.send('respond with a resource'); }); export default router; 
